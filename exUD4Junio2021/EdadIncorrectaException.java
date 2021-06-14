8b9a397ab61db48c9d092d3c31062eb4285393b2
WillTama <hetagui95@gmail.com>
2021-06-14 14:18:14 +0200

    docs: comentarios javadoc - Guillermo Tamajon

--- exUD4Junio2021/EdadIncorrectaException.java
+++ exUD4Junio2021/EdadIncorrectaException.java
@@ -1,12 +1,19 @@
 package exUD4Junio2021;
 
+/**
+ * Controla cuando una edad es incorrecta
+ * @author hetag
+ *
+ */
 public class EdadIncorrectaException extends Exception {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Crea excepciones
+	 * @param string mensaje de la excepción
+	 */
 	public EdadIncorrectaException(String string) {
 		super(string);
 	}
--- exUD4Junio2021/Empleado.java
+++ exUD4Junio2021/Empleado.java
@@ -1,11 +1,36 @@
 package exUD4Junio2021;
 
+/**
+ * Representa empleados con sus características
+ * @author hetag
+ *
+ */
 public class Empleado implements Comparable<Empleado> {
+    /**
+     * Nombre del empleado
+     */
 	public String nombre;
+	/**
+	 * Apellido del empleado
+	 */
 	public String apellido;
+	/**
+	 * Edad del empleado
+	 */
 	public int edad;
+	/**
+	 * Salario del empleado
+	 */
 	public double salario;
 
+	/**
+	 * Crea empleados
+	 * @param nombre nombre del empleado
+	 * @param apellido apellido del empleado
+	 * @param edad edad del empleado
+	 * @param salario salario del empleado
+	 * @throws EdadIncorrectaException controla edades incorrectas
+	 */
 	public Empleado(String nombre, String apellido, int edad, double salario) throws EdadIncorrectaException {
 		this.nombre = nombre;
 		this.apellido = apellido;
@@ -13,6 +38,11 @@ public class Empleado implements Comparable<Empleado> {
 		this.salario = salario;
 	}
 
+	/**
+	 * Añade una edad al empleado
+	 * @param edad edad del empleado
+	 * @throws EdadIncorrectaException controla edades incorrectas
+	 */
 	private void setEdad(int edad) throws EdadIncorrectaException {
 		if (edad <= 1 || edad >= 100) {
 			throw new EdadIncorrectaException("La edad debe ser un valor entre 1 y 100");
@@ -20,22 +50,43 @@ public class Empleado implements Comparable<Empleado> {
 		this.edad = edad;
 	}
 
+	/**
+	 * Devuelve el nombre del empelado
+	 * @return nombre del empelado
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Devuelve el apellido del empelado
+	 * @return apellido del empelado
+	 */
 	public String getApellido() {
 		return apellido;
 	}
 
+	/**
+	 * Devuelve la edad del empelado
+	 * @return edad del empleado
+	 */
 	public int getEdad() {
 		return edad;
 	}
 
+	/**
+	 * Devuelve el salario del empleado
+	 * @return salario del empleado
+	 */
 	public double getSalario() {
 		return salario;
 	}
 
+	/**
+	 * 
+	 * @param sueldoPlus añade plus al salario
+	 * @return true si el empleado tiene más de 40 años, false si es menor
+	 */
 	public boolean plus(double sueldoPlus) {
 		if (edad < 40)
 			return false;
@@ -44,12 +95,18 @@ public class Empleado implements Comparable<Empleado> {
 		return true;
 	}
 
+	/**
+	 * Devuelve el empleado en forma de cadena con sus atributos
+	 */
 	@Override
 	public String toString() {
 		return "Empleado [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", salario=" + salario
 				+ "]";
 	}
 
+	/**
+	 * Compara un empleado con otro, teniendo en cuenta la edad
+	 */
 	@Override
 	public int compareTo(Empleado other) {
 		if (this.edad == other.getEdad())
